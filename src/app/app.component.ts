27347be396871ad02497d10c5cfa8a6031073ed3
Deborah <deborah.douillet@aum.bio>
2023-08-17 15:12:01 +0200

    ngfor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,56 @@ import {PostModel} from "./models/post.model";
 })
 export class AppComponent implements OnInit{
   title = 'instaclone';
-  public post1!:PostModel
-  public post2!:PostModel
-  public post3!:PostModel
+  public posts!:PostModel[]
+
+
 
   ngOnInit(){
-  this.post1 = new PostModel(
-    'mon titre',
-    'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
-    'Chippie',
-    'la star',
-      new Date(),
-    'https://cdn.pixabay.com/photo/2019/05/08/21/21/cat-4189697_1280.jpg',
-    6,
-  )
-    this.post2 = new PostModel(
-      'ohhh',
-      'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
-      'Chippie',
-      'ohhh',
-      new Date(),
-      'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_1280.jpg',
-      12,
-    )
-    this.post3 = new PostModel(
-      'oui ?',
-      'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
-      'Chippie',
-      'oui ? ',
-      new Date(),
-      'https://cdn.pixabay.com/photo/2013/05/30/18/21/cat-114782_1280.jpg',
-      24,
-    )
+  this.posts = [
+    {
+      id:0,
+      title: 'mon titre',
+      profilPicture: 'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
+      author: 'Chippie',
+      description: 'la star',
+      createdDate: new Date(),
+      img: 'https://cdn.pixabay.com/photo/2019/05/08/21/21/cat-4189697_1280.jpg',
+      like: 6,
+      location: 'Lyon'
+    },
+    {
+      id:1,
+      title: 'ohhh',
+      profilPicture:'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
+      author:'Chippie',
+      description:'ohhh',
+      createdDate:new Date(),
+      img:'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_1280.jpg',
+      like: 12,
+    },
+    {
+      id:2,
+      title: 'oui ?',
+      profilPicture:'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
+      author:'Chippie',
+      description:'oui ? ',
+      createdDate:new Date(),
+      img:'https://cdn.pixabay.com/photo/2013/05/30/18/21/cat-114782_1280.jpg',
+      like:  24,
+    },
+    {
+      id:3,
+      title: 'mon titre',
+      profilPicture: 'https://cdn.pixabay.com/photo/2021/07/13/11/34/cat-6463284_960_720.jpg',
+      author: 'Chippie',
+      description: 'la star',
+      createdDate: new Date(),
+      img: 'https://cdn.pixabay.com/photo/2019/05/08/21/21/cat-4189697_1280.jpg',
+      like: 6,
+      location: 'Lyon'
+    },
+  ]
+
 
 
   }
